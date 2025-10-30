8f07d93597b1d7cbe760cc8527c3be1f080132d3
elizadevops <elizadevops@gmail.com>
2025-10-30 03:21:27 +0000

    new changes

---
+++ node3tier-api/src/server.js
@@ -0,0 +1,7 @@
+// src/server.js
+const app = require('./app'); // твой app.js (экспонирует Express app)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on port ${PORT}`);
+});
